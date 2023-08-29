aa063c9d66ac6524258943f687b9296dcb5a75be
Alex Wicks <alex@awicks.io>
2023-08-29 23:57:57 +0100

    feat(python): configure python lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,10 +3,10 @@ return {
   { import = "lazyvim.plugins.extras.lang.docker" },
   { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.python-semshi" },
   { import = "lazyvim.plugins.extras.lang.terraform" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
-
 
   --[[
   {
@@ -21,5 +21,4 @@ return {
     },
   },
   --]]
-
 }
