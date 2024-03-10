f73d0950354807046b33c7dd4df4a976155850d6
Alex Wicks <alex@awicks.io>
2024-03-10 12:06:29 +0000

    feat(yaml): configure ansible unsafe tag

--- lua/plugins/yaml.lua
+++ lua/plugins/yaml.lua
@@ -7,7 +7,10 @@ return {
           settings = {
             yaml = {
               customTags = {
-                "!reference sequence"
+                -- https://docs.gitlab.com/16.9/ee/ci/yaml/yaml_optimization.html#reference-tags
+                "!reference sequence",
+                -- https://docs.ansible.com/ansible/9/playbook_guide/playbooks_advanced_syntax.html#unsafe-or-raw-strings
+                "!unsafe scalar",
               },
             },
           },
