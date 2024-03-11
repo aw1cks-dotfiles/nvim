d06a93da24ddc4a0d01599ae43d34592d5ce02fb
Alex Wicks <alex@awicks.io>
2024-03-11 08:32:38 +0000

    feat(ansible): add lua ftdetect config

---
+++ ftdetect/ansible.lua
@@ -0,0 +1,7 @@
+-- https://github.com/mfussenegger/nvim-ansible/issues/7
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  pattern = {"*/playbooks/*.yml", "*/playbooks/*.yaml", "*/roles/*/tasks/*.yml", "*/roles/*/tasks/*.yaml", "*/roles/*/handlers/*.yml", "*/roles/*/handlers/*.yaml"},
+  callback = function()
+    vim.bo.filetype = "yaml.ansible"
+  end,
+})
