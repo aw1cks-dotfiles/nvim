308481a0c16cd2d5fa58555dedb5e475f7365571
Alex Wicks <alex@awicks.io>
2025-07-11 09:24:54 +0100

    feat: adding sql

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.omnisharp" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
